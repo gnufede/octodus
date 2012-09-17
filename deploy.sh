4802b82359a1bd76d414e1b34423f51da21072d0
Fede Mon <gnu.fede@gmail.com>
2012-09-17 20:59:15 +0200

    modifieded deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rsync -az /home/efdigital/lib/python2.6/fbone-0.1-py2.6.egg/fbone/static/offers/ ~/offers
-cd webapps/fbone/htdocs;
+cd ~/webapps/fbone/htdocs;
 git pull;
 ~/webapps/fbone/apache/bin/stop
 python2.6 setup.py install

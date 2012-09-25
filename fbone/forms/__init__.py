85202b9b1b5f1ec3016d7ba0cd4145c9607e981a
Fede <gnu.fede@gmail.com>
2012-09-25 11:38:32 +0200

    edit pages

--- fbone/forms/__init__.py
+++ fbone/forms/__init__.py
@@ -3,5 +3,5 @@
 from fbone.forms.frontend import (SignupForm, LoginForm, RecoverPasswordForm,
                                   ChangePasswordForm, ReauthForm)
 from fbone.forms.user import (EditDatosForm, UserAppointmentForm, UserOfferForm)
-from fbone.forms.admin import (NewGroupForm, EditProcesoForm, NewProjectForm, NewOfferForm, SetOfferForm)
+from fbone.forms.admin import (NewGroupForm, EditPageForm, NewProjectForm, NewOfferForm, SetOfferForm)
 from fbone.forms.session import (EditSessionForm, SetSessionForm)
